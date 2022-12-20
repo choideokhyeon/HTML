6e0bbe59f44e37e80136ea45186ac241d1502fa1
choideokhyeon <feelingatmosphere6088@gmail.com>
2022-12-20 17:11:28 +0900

    221220

--- prac/js/support.js
+++ prac/js/support.js
@@ -1,3 +1,4 @@
+// 파일첨부 및 미리보기
 function readURL(input) {
   if (input.files && input.files[0]) {
     var reader = new FileReader();
@@ -11,17 +12,21 @@ function readURL(input) {
 }
 
 
+
+// 문의유형 select와 option 동적 할당
 var mainSelect = document.querySelector('#select1');
 
 mainSelect.onchange = function () {
   var subSelect = document.querySelector('#select2');
   var mainOption = mainSelect.options[mainSelect.selectedIndex].value;
 
+  //select1의 선택에 따른 배열선언
   var subOptions = {
     inquiry: ['건의하기', '칭찬하기', '복구신청'],
     report: ['불법프로그램신고', '불량유저신고', '게시물신고']
   }
   
+  //select1에 따라 select2의 선택자 부여
   if(mainOption == "inquiry")
   {
     subSelect.options.length = 0;
@@ -43,7 +48,6 @@ mainSelect.onchange = function () {
       subSelect.append(option);
     }
   }
-  
 }
 
 
