6c6c1ae65d3713e5f31247beb533cc6462bc2665
choideokhyeon <feelingatmosphere6088@gmail.com>
2022-12-21 14:58:32 +0900

    221221

--- prac/js/faq.js
+++ prac/js/faq.js
@@ -2,26 +2,26 @@ var textEl = document.getElementsByClassName("text");
 
 var textpEl = document.getElementsByClassName("text-p");
 
-      function handleClick(event) {
-        console.log(event.target);
+function handleClick(event) {
+  console.log(event.target);
 
-        console.log(event.target.classList);
+  console.log(event.target.classList);
 
-        if (event.target.classList[1] === "clicked") {
-          event.target.classList.remove("clicked");
-        } else {
-          for (var i = 0; i < textEl.length; i++) {
-            textEl[i].classList.remove("clicked");
-          }
+  if (event.target.classList[1] === "clicked") {
+    event.target.classList.remove("clicked");
+  } else {
+    for (var i = 0; i < textEl.length; i++) {
+      textEl[i].classList.remove("clicked");
+    }
 
-          event.target.classList.add("clicked");
-        }
-      }
+    event.target.classList.add("clicked");
+  }
+}
 
-      function init() {
-        for (var i = 0; i < textEl.length; i++) {
-          textEl[i].addEventListener("click", handleClick);
-        }
-      }
+function init() {
+  for (var i = 0; i < textEl.length; i++) {
+    textEl[i].addEventListener("click", handleClick);
+  }
+}
 
-      init();
+init();
